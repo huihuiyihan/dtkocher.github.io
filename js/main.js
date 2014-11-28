4d8c0ae75024d2cd6a076b33ca070bb7f4d8a0bd
dtkocher <dtkocher@gmail.com>
2014-11-28 16:05:04 -0500

    Finish header stuff so now looks perfect. :)

--- js/main.js
+++ js/main.js
@@ -4,13 +4,18 @@ $(function() {
     $(window).scroll(function() {
       if ($(window).scrollTop() > $(window).height()-200) {
         $(".background").addClass("fade-out");
+        $(".background-bottom").addClass("fade-out");
       }
       else if ($(window).scrollTop() > ($(window).height()-400)) {
         $(".background").addClass("fade");
         $(".background").removeClass("fade-out");
+
+        $(".background-bottom").addClass("fade");
+        $(".background-bottom").removeClass("fade-out");
       }
       else if ($(window).scrollTop() < ($(window).height()-400)) {
         $(".background").removeClass("fade");
+        $(".background-bottom").removeClass("fade");
       }
     });
   }
