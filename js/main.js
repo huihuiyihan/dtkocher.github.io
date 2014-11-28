c564ba89238d3107c56af082c9500bec10292a04
dtkocher <dtkocher@gmail.com>
2014-11-28 14:05:43 -0500

    Fix up the reset of the background stuff

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 $(function() {
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > $(window).height()-200) {
-      $(".background").addClass("fade-out");
-    }
-    else if ($(window).scrollTop() > ($(window).height()-400)) {
-      $(".background").addClass("fade");
-      $(".background").removeClass("fade-out");
-    }
-    else if ($(window).scrollTop() < ($(window).height()-400)) {
-      $(".background").removeClass("fade");
-    }
-  });
+  window.sr = new scrollReveal();
+  if($(".background").length > 0) {
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > $(window).height()-200) {
+        $(".background").addClass("fade-out");
+      }
+      else if ($(window).scrollTop() > ($(window).height()-400)) {
+        $(".background").addClass("fade");
+        $(".background").removeClass("fade-out");
+      }
+      else if ($(window).scrollTop() < ($(window).height()-400)) {
+        $(".background").removeClass("fade");
+      }
+    });
+  }
 });
-
-window.sr = new scrollReveal();
