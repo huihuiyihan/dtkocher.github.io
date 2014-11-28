b8f89c352426028d8964a8832f7c11eb311812b0
dtkocher <dtkocher@gmail.com>
2014-11-28 13:54:27 -0500

    Add scrollReveal and fix up some of the transition stuff.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(function() {
   $(window).scroll(function() {
-    if ($(window).scrollTop() > ($(window).height()-300)) {
-      $(".background").addClass("fade");
+    if ($(window).scrollTop() > $(window).height()-200) {
+      $(".background").addClass("fade-out");
     }
-    else {
+    else if ($(window).scrollTop() > ($(window).height()-400)) {
+      $(".background").addClass("fade");
+      $(".background").removeClass("fade-out");
+    }
+    else if ($(window).scrollTop() < ($(window).height()-400)) {
       $(".background").removeClass("fade");
     }
   });
 });
+
+window.sr = new scrollReveal();
